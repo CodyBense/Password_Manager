16c7af0b1475ff4b94659493d1d5f8dc908e9be6
CodyBense <codybense@gmail.com>
2025-04-28 19:53:55 -0400

    seperated login info to own page

--- app.py
+++ app.py
@@ -13,38 +13,13 @@ import os
 
 def setup_page():
     """ Sets up the streamlit page. """
-    st.title('The Vault')
-    # st.button('Add login')
+    the_vault = st.Page("pages/homepage.py", title="The Vault")
     add_login_page = st.Page("pages/add_login.py", title="Add Login")
-    pg = st.navigation([st.Page("homepage.py"), add_login_page])
-
-def fetch_data():
-    """ Accesses the data from the sql database. """
-    try:
-        #Gets the info from the .env file.
-        load_dotenv()
-        user = os.getenv("DB_USER")
-        password = os.getenv("DB_PASSWORD")
-        host = os.getenv("DB_HOST")
-        port = os.getenv("DB_PORT")
-        db = os.getenv("DB_NAME")
-
-        # Connects to the database.
-        engine = create_engine(f"mysql+mysqldb://{user}:{password}@{host}:{port}/{db}")
-
-        # Queries the database for all the login infor and prints it to the page.
-        query = "SELECT * FROM login_info;"
-        df = pd.read_sql(query, engine)
-    except Exception as e:
-        print(f'failed fetching data: {e}')
-    return df
-
+    pg = st.navigation([the_vault, add_login_page])
+    pg.run()
 
 def main():
     setup_page()
-    # df = fetch_data()
-    #
-    # st.dataframe(df, use_container_width=True, hide_index=True)
 
 
 if __name__ == "__main__":
