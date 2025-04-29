d463ce99e83101830aa6006d0c2717a128f77f4a
CodyBense <codybense@gmail.com>
2025-04-28 20:51:13 -0400

    stated delete login

---
+++ pages/delete_login.py
@@ -0,0 +1,18 @@
+"""
+    TODO:
+        * Implement delte query
+"""
+import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+def main():
+    st.title('Delete Login')
+
+
+if __name__ == "__main__":
+    main()
